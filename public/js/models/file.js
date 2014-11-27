40ec7b977329d2148d103acf8652723d0edb8591
Renaud Martinet <karouf@gmail.com>
2014-11-27 17:41:03 +0100

    Display movie corresponding to each file

--- public/js/models/file.js
+++ public/js/models/file.js
@@ -1,22 +1,27 @@
 Homeflix.File = DS.Model.extend({
   name: DS.attr('string'),
+  movie: DS.belongsTo('movie', {async: true}),
 });
 
 Homeflix.File.FIXTURES = [
   {
   id: 1,
     name: 'napoleon-dynamite.mp4',
+    movie: null,
   },
   {
   id: 2,
     name: '[YEAH].any.given.sunday-FR.mkv',
+    movie: null,
   },
   {
   id: 3,
     name: 'xor-lgd-lca.avi',
+    movie: null,
   },
   {
   id: 4,
     name: 'liberte-oleron-filmdevacances.avi',
+    movie: 1,
   },
 ];
