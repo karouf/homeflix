78393c83927205d0f1269b0e79075309c8a5f8c6
Renaud Martinet <karouf@gmail.com>
2014-11-27 14:00:16 +0100

    More example covers

--- public/js/controllers/movie_controller.js
+++ public/js/controllers/movie_controller.js
@@ -1,6 +1,6 @@
 Homeflix.MovieController = Ember.ObjectController.extend({
   coverUrl: function() {
     var model = this.get('model');
-    return '/img/' + model.get('cover');
+    return '/assets/covers/' + model.get('cover');
   }.property('model.cover')
 });
--- public/js/models/movie.js
+++ public/js/models/movie.js
@@ -7,16 +7,41 @@ Homeflix.Movie.FIXTURES = [
   {
   id: 1,
     title: 'Liberté-Oléron',
-    cover: 'cover-placeholder.gif',
+    cover: 'liberteoleron.jpg',
   },
   {
   id: 2,
     title: 'La cité de la peur',
-    cover: 'cover-placeholder.gif',
+    cover: 'lacitedelapeur.jpg',
   },
   {
   id: 3,
     title: 'Predator',
-    cover: 'cover-placeholder.gif',
-  }
+    cover: 'predator.jpg',
+  },
+  {
+  id: 4,
+    title: 'Home alone',
+    cover: 'homealone.jpg',
+  },
+  {
+  id: 5,
+    title: "Don't be a menace to South Central while drinking your juice in the hood",
+    cover: 'dontbeamenace.jpg',
+  },
+  {
+  id: 6,
+    title: 'Dumb & dumber',
+    cover: 'dumbanddumber.jpg',
+  },
+  {
+  id: 7,
+    title: 'Rambo',
+    cover: 'rambo.jpg',
+  },
+  {
+  id: 8,
+    title: 'Snatch',
+    cover: 'snatch.jpg',
+  },
 ];
