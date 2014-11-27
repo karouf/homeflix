d1176448f3a6c545ef03d3ef29080b61cf320255
Renaud Martinet <karouf@gmail.com>
2014-11-27 16:53:33 +0100

    Display files

---
+++ public/js/models/file.js
@@ -0,0 +1,18 @@
+Homeflix.File = DS.Model.extend({
+  name: DS.attr('string'),
+});
+
+Homeflix.File.FIXTURES = [
+  {
+  id: 1,
+    name: 'napoleon-dynamite.mp4',
+  },
+  {
+  id: 2,
+    name: '[YEAH].any.given.sunday-FR.mkv',
+  },
+  {
+  id: 3,
+    name: 'xor-lgd-lca.avi',
+  },
+];
--- public/js/router.js
+++ public/js/router.js
@@ -1,9 +1,16 @@
 Homeflix.Router.map(function() {
   this.resource('movies', { path: '/' });
+  this.resource('files', { path: '/files' });
 });
 
 Homeflix.MoviesRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('movie');
+  }
+});
+
+Homeflix.FilesRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('file');
   }
 });
