db2f33c7d3c31dc26a7df642ffecc1a7f486dc54
Renaud Martinet <karouf@gmail.com>
2014-11-27 17:20:54 +0100

    Display a single movie

--- public/js/router.js
+++ public/js/router.js
@@ -1,11 +1,18 @@
 Homeflix.Router.map(function() {
   this.resource('movies', { path: '/' });
+  this.resource('movie', { path: '/movie/:movie_id' });
   this.resource('files', { path: '/files' });
 });
 
 Homeflix.MoviesRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('movie');
+  }
+});
+
+Homeflix.MovieRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('movie', params.movie_id);
   }
 });
 
